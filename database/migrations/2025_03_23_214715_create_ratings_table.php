72a7163abbc3852c09a494e4a65e49b6286be55f
Mohamed-Hmidouch <ffa54202@gmail.com>
2025-04-07 14:05:27 +0200

    create migrations rating

--- database/migrations/2025_03_23_214715_create_ratings_table.php
+++ database/migrations/2025_03_23_214715_create_ratings_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('ratings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('content_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('value');
             $table->timestamps();
         });
     }

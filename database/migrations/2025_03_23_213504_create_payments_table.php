7d5f3b45b324c4614c5e5e3fce4bfb8d1678d788
Mohamed-Hmidouch <ffa54202@gmail.com>
2025-04-07 13:58:53 +0200

    create migrations payement

--- database/migrations/2025_03_23_213504_create_payments_table.php
+++ database/migrations/2025_03_23_213504_create_payments_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('subscription_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 8, 2);
+            $table->date('date');
+            $table->string('status');
+            $table->string('payment_method_type');
+            $table->unsignedBigInteger('payment_method_id');
             $table->timestamps();
         });
     }

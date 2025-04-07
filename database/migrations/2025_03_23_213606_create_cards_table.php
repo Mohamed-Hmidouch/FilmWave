a06eb915f9125fb97e937cbf9c91c5f0a975209a
Mohamed-Hmidouch <ffa54202@gmail.com>
2025-04-07 14:03:15 +0200

    create migrations carte

--- database/migrations/2025_03_23_213606_create_cards_table.php
+++ database/migrations/2025_03_23_213606_create_cards_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('cards', function (Blueprint $table) {
             $table->id();
+            $table->string('card_number');
+            $table->string('expiry_date');
+            $table->string('cvv');
             $table->timestamps();
         });
     }
